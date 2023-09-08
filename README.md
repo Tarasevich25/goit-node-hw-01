368f02c5d6208fc5880d7ae79bf343b3d2e3f23d
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-09-08 12:08:46 +0300

    finished hw

---
+++ README.md
@@ -0,0 +1,12 @@
+# goit-node-hw-01
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://monosnap.com/file/t2K2ZnHuXQIyD2NHWCKlOgQMtimlVF
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+https://monosnap.com/file/bLdYJHZ5G2IEROgYwSJCAx5UgoWe5J
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+https://monosnap.com/file/lEkoBOnVsQsgZPaJZVjcWUcTZYoW6M
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+https://monosnap.com/file/6YuUl2r7b6IQmGH3uezzxEbD4zdJbB
---
+++ contacts.js
@@ -0,0 +1,68 @@
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(contact => contact.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  const index = contacts.findIndex(
+    contact => contact.id === contactId
+  );
+
+  if (index === -1) {
+    console.log(`null`);
+    return null;
+  }
+
+  const removedContact = contacts.splice(index, 1)[0];
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const newContact = { name, email, phone };
+  const existingData = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(existingData);
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function updateContact(contactId, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === id);
+
+  if (index === -1) {
+    console.log(null);
+    return null;
+  }
+
+  const updatedContact = {
+    id: contactId,
+    ...contacts[index],
+    ...data,
+  };
+  contacts[index] = updatedContact;
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return updatedContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
